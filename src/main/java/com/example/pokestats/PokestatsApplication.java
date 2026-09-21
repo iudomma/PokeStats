7f134914a2379bce3ba8360c3bdfa58f48a698a8
iudomma <iu.d.martintorre@gmail.com>
2026-09-21 08:50:41 +0200

    Upload base project

---
+++ src/main/java/com/example/pokestats/PokestatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokestatsApplication.class, args);
+	}
+
+}
